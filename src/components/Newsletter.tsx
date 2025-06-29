e55178ae7bab2cc3eed2637574aa14c2d4e95476
ola <atobateleameer@gmail.com>
2025-06-29 08:15:15 +0100

    Fix: Escaped apostrophes for Vercel build2

--- src/components/Newsletter.tsx
+++ src/components/Newsletter.tsx
@@ -8,12 +8,12 @@ export const Newsletter = () => {
             Are you passionate about programming and always looking for ways to learn more and stay up-to-date with the latest developments in the field? If so, our programming blog newsletter is the perfect fit for you!
           </p>
           <p className="text-brand-gray">
-            With our newsletter, you'll receive regular updates on the hottest topics in the world of programming, as well as exclusive access to content that you won't find anywhere else.
+            With our newsletter, you&apos;ll receive regular updates on the hottest topics in the world of programming, as well as exclusive access to content that you won&apos;t find anywhere else.
           </p>
         </div>
         <div className="space-y-4">
-            <p className="text-brand-gray">Plus, our team of expert writers and industry professionals provide in-depth analysis and commentary that you're sure you're only getting the most reliable and relevant information.</p>
-            <p className="text-brand-gray">Don't miss out - sign up for our newsletter today and take your programming knowledge to the next level!</p>
+            <p className="text-brand-gray">Plus, our team of expert writers and industry professionals provide in-depth analysis and commentary that you&apos;re sure you&apos;re only getting the most reliable and relevant information.</p>
+            <p className="text-brand-gray">Don&apos;t miss out - sign up for our newsletter today and take your programming knowledge to the next level!</p>
             <form className="flex space-x-2 mt-4">
                 <input type="email" placeholder="Register newsletter" className="flex-grow bg-[#010409] border border-brand-border rounded-md px-4 py-2 focus:outline-none focus:ring-2 focus:ring-brand-blue" />
                 <button type="submit" className="bg-brand-blue text-white font-semibold px-6 py-2 rounded-md hover:opacity-90 transition-opacity">Submit</button>
